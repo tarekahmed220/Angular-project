76a60c8ba44f342fcd7daf11cbf900b0a5a24477
tarekahmed220 <tarek.ahm12@gmail.com>
2024-08-02 18:02:23 +0300

    complete ui of navbar and userList and fetch the data

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { userInterface, UsersResponse } from '../interface/userInterface';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  users: userInterface[] = [];
+  private apiUrl = 'https://reqres.in/api/users';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllUsers(page: number = 1): Observable<userInterface[]> {
+    return this.http.get<UsersResponse>(`${this.apiUrl}?page=${page}`).pipe(
+      map((response) => {
+        this.users = response.data;
+        return this.users;
+      })
+    );
+  }
 }
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { userInterface } from '../interface/userInterface';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
   imports: [],
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.css'
+  styleUrl: './users-list.component.css',
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  users!: userInterface;
+  constructor(private _userServ: UsersService) {}
 
+  ngOnInit(): void {
+    const spans = document.querySelectorAll<HTMLSpanElement>(
+      '.pagePagination span'
+    );
+    spans.forEach((span) => {
+      span.addEventListener('click', () => {
+        spans.forEach((s) => {
+          s.classList.remove('active');
+        });
+        span.classList.add('active');
+      });
+    });
+
+    this._userServ.getAllUsers(1).subscribe((users) => {
+      console.log(users);
+    });
+  }
 }
